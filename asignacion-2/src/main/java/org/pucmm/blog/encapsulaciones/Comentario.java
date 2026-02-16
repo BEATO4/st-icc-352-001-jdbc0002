73d082ec854a607277a429708e40cab8851104e3
BEATO4 <jostin.beato4@gmail.com>
2026-02-16 08:38:28 -0400

    HTML faltantes utilizando Thymeleaf y Bootstrap 5 agregado el plugin de aplication en build.gradle.kts

--- asignacion-2/src/main/java/org/pucmm/blog/encapsulaciones/Comentario.java
+++ asignacion-2/src/main/java/org/pucmm/blog/encapsulaciones/Comentario.java
@@ -1,4 +1,38 @@
 package org.pucmm.blog.encapsulaciones;
 
 public class Comentario {
+
+    private long id;
+    private String comentario;
+    private Usuario autor;
+
+    public Comentario(long id, String comentario, Usuario autor) {
+        this.id = id;
+        this.comentario = comentario;
+        this.autor = autor;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
