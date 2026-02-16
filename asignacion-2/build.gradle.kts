04060c52d7a09cd1bd4596e06c1445924808c38f
BEATO4 <jostin.beato4@gmail.com>
2026-02-16 08:59:57 -0400

    agregadas dependencias faltantes

--- asignacion-2/build.gradle.kts
+++ asignacion-2/build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("io.javalin:javalin:6.7.0")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 
 }
 
