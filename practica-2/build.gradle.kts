1139b2a05424317a463869616e056cf6fb1edd42
BEATO4 <jostin.beato4@gmail.com>
2026-02-16 21:07:04 -0400

    Moviendo carpeta desde el repositorio anterior

---
+++ practica-2/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    id ("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    // Reemplaza con tu paquete y clase principal
+    mainClass.set("\"org.pucmm.blog.Main\"")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ practica-2/src/main/java/org/pucmm/blog/Main.java
@@ -0,0 +1,69 @@
+package org.pucmm.blog;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.pucmm.blog.encapsulaciones.*;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinThymeleaf; //motor de plantillas
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+    // 1. Persistencia en Memoria
+    private static List<Usuario> usuarios = new ArrayList<>();
+    private static List<Articulo> articulos = new ArrayList<>();
+    private static long articuloIdCounter = 1;
+    private static long comentarioIdCounter = 1;
+
+    public static void main(String[] args) {
+
+        // 2. Crear usuario admin por defecto al iniciar
+        usuarios.add(new Usuario("admin", "Administrador", "admin", true, true));
+        // Usuario  prueba autor
+        usuarios.add(new Usuario("autor", "Autor Demo", "1234", false, true));
+
+        // Datos para probar la vista
+        Articulo a1 = new Articulo(articuloIdCounter++, "Primer Post", "Este es el cuerpo del artículo que debe ser largo para probar el resumen de 70 caracteres que pide la práctica.", usuarios.get(0));
+        a1.getListaEtiquetas().add(new Etiqueta(1, "Tecnologia"));
+        articulos.add(a1);
+
+        Javalin app = Javalin.create(config -> {
+            // Configurar archivos
+            config.staticFiles.add("/public", Location.CLASSPATH);
+            config.fileRenderer(new JavalinThymeleaf());
+        }).start(7000);
+
+        //FILTROS DE SEGURIDAD
+        app.before("/admin/*", ctx -> {
+            Usuario usuario = ctx.sessionAttribute("usuario");
+            if (usuario == null) {
+                ctx.redirect("/login");
+            }
+        });
+
+        app.before("/crear-articulo", ctx -> {
+            Usuario usuario = ctx.sessionAttribute("usuario");
+            // Solo admin o autor pueden crear [cite: 61, 69]
+            if (usuario == null || (!usuario.isAdministrator() && !usuario.isAutor())) {
+                ctx.redirect("/login");
+            }
+        });
+
+        //RUTAS
+        app.get("/", ctx -> {
+            Map<String, Object> modelo = new HashMap<>();
+
+            articulos.sort((a, b) -> b.getFecha().compareTo(a.getFecha()));
+
+            modelo.put("articulos", articulos);
+            modelo.put("usuario", ctx.sessionAttribute("usuario"));
+
+            ctx.render("/templates/index.html", modelo);
+        });
+
+        app.get("/login", ctx -> ctx.render("/templates/login.html"));
+
+    }
+}
